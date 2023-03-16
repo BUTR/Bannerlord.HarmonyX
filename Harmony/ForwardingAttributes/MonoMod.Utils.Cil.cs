91ab45d7f57bcda2c6fcb937bfe5a4e8c1bb11fc
Vitalii Mikhailov <vitalii.mikhailov@amdaris.com>
2023-03-16 14:46:40 +0200

    Added MonoMod.* type forwarding

---
+++ Harmony/ForwardingAttributes/MonoMod.Utils.Cil.cs
@@ -0,0 +1,8 @@
+#if NETFRAMEWORK
+using System.Runtime.CompilerServices;
+
+[assembly: TypeForwardedTo(typeof(MonoMod.Utils.Cil.CecilILGenerator))]
+[assembly: TypeForwardedTo(typeof(MonoMod.Utils.Cil.ILGeneratorShim))]
+[assembly: TypeForwardedTo(typeof(MonoMod.Utils.Cil.CecilILGenerator))]
+[assembly: TypeForwardedTo(typeof(MonoMod.Utils.Cil.CecilILGenerator))]
+#endif
